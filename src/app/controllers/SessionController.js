a5aa1a9c9721f5cf1c44cb95d83f4e86c4614a6b
kelitasukita <kelitasukita@gmail.com>
2019-10-21 02:25:17 +0200

    Validacao do cadastro de usuário

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,20 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/user';
+import * as Yup from 'yup';
 
+import User from '../models/user';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+
+    if(!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
--- src/app/controllers/UsuarioController.js
+++ src/app/controllers/UsuarioController.js
@@ -1,7 +1,18 @@
+import * as Yup from 'yup';
 import User from '../models/user';
 
 class UsuarioController {
  async store(req, res) {
+   const schema = Yup.object().shape({
+     name: Yup.string().required(),
+     email: Yup.string().email().required(),
+     password: Yup.string().min(6).required(),
+   });
+
+   if(!(await schema.isValid(req.body))) {
+     return res.status(400).json({ error: 'Falha na validação' });
+   }
+
    const verificaUsuario = await User.findOne({ where: { email: req.body.email } });
 
    if(verificaUsuario) {
@@ -26,6 +37,23 @@ class UsuarioController {
  }
 
  async update(req, res) {
+  const schema = Yup.object().shape({
+    name: Yup.string(),
+    email: Yup.string().email(),
+    oldPassword: Yup.string().min(6),
+    password: Yup.string().min(6).when('oldPassword', (oldPassword, field) =>
+      oldPassword ? field.required() : field
+    ),
+    confirmPassword: Yup.string().when('password', (password, field) =>
+      password ? field.required().oneOf([Yup.ref('password')]) : field
+    ),
+
+  });
+
+  if(!(await schema.isValid(req.body))) {
+    return res.status(400).json({ error: 'Falha na validação' });
+  }
+
   const { email, oldPassword } = req.body;
 
   const usuario = await User.findByPk(req.idUsuario);
