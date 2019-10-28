f502a486aaafbe1ace0a35f6dd62073b681f4395
kelitasukita <kelitasukita@gmail.com>
2019-10-28 13:35:11 +0100

    Configuracão Multer e criação de rotas de arquivo

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import multerConfig from './config/multer';
 import UsuarioController from './app/controllers/UsuarioController';
 import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const rotas = new Router();
+const upload = multer(multerConfig);
 
 rotas.post('/users', UsuarioController.store);
 rotas.post('/sessions', SessionController.store);
@@ -13,5 +16,9 @@ rotas.post('/sessions', SessionController.store);
 rotas.use(authMiddleware);
 
 rotas.put('/users', UsuarioController.update);
+
+rotas.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default rotas;
