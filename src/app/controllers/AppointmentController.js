0015c29a2c7091d34994b6c00f4bc66446dd6858
kelitasukita <kelitasukita@gmail.com>
2019-10-31 13:42:58 +0100

    Validações do agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import User from '../models/User';
 import Appointment from '../models/Appointment';
@@ -18,20 +19,50 @@ class AppointmentController {
 
     /* Check se o id informado é de um provedor de serviços */
 
-    const isProvider = await User.findOne({
+    const checkIsProvider = await User.findOne({
       where: { id: provider_id, provider: true }
     });
 
-    if (!isProvider) {
+    if (!checkIsProvider) {
       return res.status(401).json({
-        error: 'Você só pode fazer um agendamento com um provedor de serviços'
+        error: 'Você só pode fazer agendamento com um provedor de serviços'
       });
+    }
+
+    /*
+      Check se a data informada é uma data passada
+    */
+
+    const hourStart = startOfHour(parseISO(date)); // Essa variável vai guardar uma string com o date que será
+    // transformado num dado do JS através do parseISO e o startOfHour vai fazer com q todos os horários serjam
+    // arredondados para hora certa, não aceitando minutos, mas somente a hora redonda.
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Data passada, não permitida' });
+    }
+
+    /*
+      Check se o Provider já não possui agendamento para data e dia informado
+    */
+
+    const checkDisponibilidade = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart
+      }
+    });
+
+    if (checkDisponibilidade) {
+      return res
+        .status(400)
+        .json({ error: 'Data de agendamento não disponível' });
     }
 
     const appointment = await Appointment.create({
       user_id: req.idUsuario, // Esse idUsuário vem do meu middleware de autenticação
       provider_id,
-      date
+      date: hourStart
     });
 
     return res.json(appointment);
