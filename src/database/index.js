af50fddbe2c571a0ae75db95e1acc1a9ba861b5f
kelitasukita <kelitasukita@gmail.com>
2019-11-03 22:49:16 +0100

    Configuração do MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -11,6 +12,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -19,6 +21,17 @@ class Database {
     models
       .map(model => model.init(this.conexao))
       .map(model => model.associate && model.associate(this.conexao.models)); // Estou chamando o método static associate
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true
+      }
+    );
   }
 }
 
