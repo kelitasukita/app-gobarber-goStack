4bc39540192efb6ed0b2993e13a5b1b6b438c870
kelitasukita <kelitasukita@gmail.com>
2019-11-03 22:58:18 +0100

    Notificação de agendamentos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -90,6 +92,19 @@ class AppointmentController {
       user_id: req.idUsuario, // Esse idUsuário vem do meu middleware de autenticação
       provider_id,
       date: hourStart
+    });
+
+    /*
+      Notificar provedor de serviços sobre seus agendamentos
+    */
+    const user = await User.findByPk(req.idUsuario);
+    const formatedDate = format(hourStart, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+      locale: pt
+    });
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formatedDate}`,
+      user: provider_id
     });
 
     return res.json(appointment);
