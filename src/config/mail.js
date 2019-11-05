02c1fc0f0f1a4d8bdc7db82b7082926934fade96
kelitasukita <kelitasukita@gmail.com>
2019-11-05 11:31:18 +0100

    Configurando envio de emails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'b2aaf8c1532425',
+    pass: '047251303a155a'
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+};
