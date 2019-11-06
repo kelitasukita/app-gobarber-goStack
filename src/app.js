43bf0aa7a8db944ed688c989b9656c4d480c6541
kelitasukita <kelitasukita@gmail.com>
2019-11-06 15:07:48 +0100

    Tratamento de exceções

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
 
+import sentryConfig from './config/sentry';
+import 'express-async-errors';
 import rotas from './routes';
 import './database';
 
@@ -8,11 +12,15 @@ class App {
   constructor() {
     this.servidor = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.minhasRotas();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.servidor.use(Sentry.Handlers.requestHandler());
     this.servidor.use(express.json());
     this.servidor.use(
       '/files',
@@ -22,6 +30,17 @@ class App {
 
   minhasRotas() {
     this.servidor.use(rotas);
+    this.servidor.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    // Middleware de tratamentos de exceção
+    this.servidor.use(async (err, req, res, next) => {
+      // Qdo se recebe 4 paramentros no middleware o express automaticamente                                                         entende que se trata de um middleware de tratamentos de exceções.
+      const errors = await new Youch(err, req).toJSON(); // o Youch vai mostrar o erro, na linguagem que eu definir, de uma forma melhor para o programador.
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
